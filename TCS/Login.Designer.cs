dd09d789502975500d9728f97916038535ecc7c6
gabri <gabri@GT>
2017-06-04 13:33:02 -0600

    PR

---
+++ TCS/Login.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TCS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usuarioTxt = new System.Windows.Forms.TextBox();
+            this.contraseñaTxt = new System.Windows.Forms.TextBox();
+            this.ingresarBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña: ";
+            // 
+            // usuarioTxt
+            // 
+            this.usuarioTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuarioTxt.Location = new System.Drawing.Point(133, 65);
+            this.usuarioTxt.Multiline = true;
+            this.usuarioTxt.Name = "usuarioTxt";
+            this.usuarioTxt.Size = new System.Drawing.Size(100, 20);
+            this.usuarioTxt.TabIndex = 2;
+            // 
+            // contraseñaTxt
+            // 
+            this.contraseñaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contraseñaTxt.Location = new System.Drawing.Point(133, 104);
+            this.contraseñaTxt.Multiline = true;
+            this.contraseñaTxt.Name = "contraseñaTxt";
+            this.contraseñaTxt.Size = new System.Drawing.Size(100, 20);
+            this.contraseñaTxt.TabIndex = 3;
+            // 
+            // ingresarBtn
+            // 
+            this.ingresarBtn.Location = new System.Drawing.Point(102, 151);
+            this.ingresarBtn.Name = "ingresarBtn";
+            this.ingresarBtn.Size = new System.Drawing.Size(75, 23);
+            this.ingresarBtn.TabIndex = 4;
+            this.ingresarBtn.Text = "INGRESAR";
+            this.ingresarBtn.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 221);
+            this.Controls.Add(this.ingresarBtn);
+            this.Controls.Add(this.contraseñaTxt);
+            this.Controls.Add(this.usuarioTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usuarioTxt;
+        private System.Windows.Forms.TextBox contraseñaTxt;
+        private System.Windows.Forms.Button ingresarBtn;
+    }
+}
