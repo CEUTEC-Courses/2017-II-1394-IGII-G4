2ebc2e3d15d5efe33d39bb39a121ab15c9ee7b64
Gabriela Tabora <gabrielatabora@unitec.edu>
2017-06-22 14:26:50 -0600

    Singleton sin terminar

---
+++ TCS/Login/LoginSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCS.Login
+{
+    class LoginSingleton
+    {
+        private static LoginSingleton log;
+        private LoginSingleton()
+        {
+
+        }
+
+        public static LoginSingleton getInstance()
+        {
+            if (log == null)
+            {
+                log = new LoginSingleton();
+            }
+            return log;
+        }
+    }
+}
