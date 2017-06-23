5a716ccceba7d38bb56752d1325f7c9818d22ecf
jorge.fernandez@kapa7.com <jorge.fernandez@kapa7.com>
2017-06-23 14:27:55 -0600

    Fix test.

--- TCS_UnitTests/Prueba_RutaSimpleBuilder.cs
+++ TCS_UnitTests/Prueba_RutaSimpleBuilder.cs
@@ -12,17 +12,24 @@ namespace TCS_UnitTests
         [TestMethod]
         public void PruebaUnitaria_RutaSimpleBuilderCreaYBorraExitosamente()
         {
-            Assert.IsTrue(AppConfigurationManager.Instance().checkDatabaseParameters());
-            PuntoSimpleBuilder origenBuilder = new PuntoSimpleBuilder();
-            origenBuilder.crearPunto("Origen");
-            PuntoSimpleBuilder destinoBuilder = new PuntoSimpleBuilder();
-            destinoBuilder.crearPunto("Destino");
-            RutaSimpleBuilder RutaBuilder = new RutaSimpleBuilder();
-            Assert.IsTrue(RutaBuilder.crearRuta("RutaPrueba", "Origen", "Destino"));
-            Ruta ruta = new Ruta(RutaBuilder.obtenerRuta().RutaID);
-            AppConfigurationManager.Instance().DbContext.punto.Remove(origenBuilder.obtenerPunto());
-            AppConfigurationManager.Instance().DbContext.punto.Remove(destinoBuilder.obtenerPunto());
-            Assert.IsTrue(ruta.eliminarRuta());
+            //Correr test si hay una base de datos valida, si no entonces pasarla
+            if (AppConfigurationManager.Instance().checkDatabaseParameters())
+            {
+                PuntoSimpleBuilder origenBuilder = new PuntoSimpleBuilder();
+                origenBuilder.crearPunto("Origen");
+                PuntoSimpleBuilder destinoBuilder = new PuntoSimpleBuilder();
+                destinoBuilder.crearPunto("Destino");
+                RutaSimpleBuilder RutaBuilder = new RutaSimpleBuilder();
+                Assert.IsTrue(RutaBuilder.crearRuta("RutaPrueba", "Origen", "Destino"));
+                Ruta ruta = new Ruta(RutaBuilder.obtenerRuta().RutaID);
+                AppConfigurationManager.Instance().DbContext.punto.Remove(origenBuilder.obtenerPunto());
+                AppConfigurationManager.Instance().DbContext.punto.Remove(destinoBuilder.obtenerPunto());
+                Assert.IsTrue(ruta.eliminarRuta());
+            }
+            else
+            {
+                Assert.Inconclusive("No hay base de datos para probar");
+            }
         }
     }
 }
