2bc54268a4894477a5f4b8c63110ceb30855a0ca
Gabriela Tabora <gabrielatabora@unitec.edu>
2017-06-23 17:44:56 -0600

    Prueba Viajes

---
+++ TCS_UnitTests/Prueba_ListarViajesPorNumero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TCS_UnitTests
+{
+    [TestClass]
+    public class Prueba_ListarViajesPorNumero
+    {
+        [TestMethod]
+        public void listarViajesPorNumeroExitosamente()
+        {
+
+        }
+    }
+}
