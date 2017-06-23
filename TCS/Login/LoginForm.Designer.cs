fe684d0aaf774fdf2e9b5afef4791b15a4f9af28
Gabriela Tabora <gabrielatabora@unitec.edu>
2017-06-23 11:45:52 -0600

    Login

--- TCS/Login/LoginForm.Designer.cs
+++ TCS/Login/LoginForm.Designer.cs
@@ -54,6 +54,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(27, 27);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(195, 18);
